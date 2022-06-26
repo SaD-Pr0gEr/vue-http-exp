c927aeb40607cab3bcd920aaf6ced9cc3ada3a4a
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2022-06-26 17:23:39 +0500

    added server parsing firebase

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -1,37 +1,75 @@
 <template>
-  <section>
-    <base-card>
-      <h2>Submitted Experiences</h2>
-      <div>
-        <base-button>Load Submitted Experiences</base-button>
-      </div>
-      <ul>
-        <survey-result
-          v-for="result in results"
-          :key="result.id"
-          :name="result.name"
-          :rating="result.rating"
-        ></survey-result>
-      </ul>
-    </base-card>
-  </section>
+    <section>
+        <base-card>
+            <h2>Submitted Experiences</h2>
+            <div>
+                <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
+            </div>
+            <p v-if="isLoading">Loading...</p>
+            <p v-else-if="!isLoading && error">{{ error }}</p>
+            <p v-else-if="!isLoading && (!results || results.length === 0)">No data found</p>
+            <ul v-else>
+                <survey-result
+                    v-for="result in results"
+                    :key="result.id"
+                    :name="result.name"
+                    :rating="result.rating"
+                ></survey-result>
+            </ul>
+        </base-card>
+    </section>
 </template>
 
 <script>
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
-  components: {
-    SurveyResult,
-  },
+    components: {
+        SurveyResult,
+    },
+    data() {
+        return {
+            results: [],
+            isLoading: false,
+            error: null
+        }
+    },
+    methods: {
+        loadExperiences() {
+            this.isLoading = true;
+            fetch("https://vue-http-10ae1-default-rtdb.europe-west1.firebasedatabase.app/data.json")
+                .then((response) => {if (response.ok) {
+                    return response.json();
+                }})
+                .then((data) => {
+                    this.isLoading = false
+                    this.error = null
+                    const results = []
+                    for (const res in data) {
+                        results.push({
+                            id: res,
+                            name: data[res].name,
+                            rating: data[res].rating
+                        })
+                    }
+                    this.results = results;
+                }).catch((error) => {
+                    this.isLoading = false
+                    console.log(error)
+                    this.error = "Can't get data... Please try again later";
+                })
+        }
+    },
+    mounted() {
+        this.loadExperiences();
+    }
 };
 </script>
 
 <style scoped>
 ul {
-  list-style: none;
-  margin: 0;
-  padding: 0;
+    list-style: none;
+    margin: 0;
+    padding: 0;
 }
 </style>
